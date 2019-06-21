68e50474b663106bdf3a556c13b6f9d4992eda92
lipskydan <lipsky@knu.ua>
2019-06-21 13:09:44 +0300

    Finish Basic3

--- Playground/Basic3.playground/Contents.swift
+++ Playground/Basic3.playground/Contents.swift
@@ -2,3 +2,17 @@ import UIKit
 
 //: Ex: 1
 
+var str: String = "Iphone"; var char: Character = "S"
+var a = 2; var b = 3
+var strLiteral = String(str + String(a+b) + String(char))
+print(strLiteral)
+
+//: Ex: 2
+
+print("\n*   *   *\n" + " * * * * \n" + "  *   *  ")
+
+//: Ex: 3
+var data = 21; var year = 2019
+var month = " June "
+print(String(year) + month + String(data))
+
