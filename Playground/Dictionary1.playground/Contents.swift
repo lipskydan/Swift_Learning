9e657efec96d69e3bda8cbda93a335e3319e2c71
lipskydan <lipsky@knu.ua>
2019-07-07 15:23:36 +0300

    Update Contents.swift

--- Playground/Dictionary1.playground/Contents.swift
+++ Playground/Dictionary1.playground/Contents.swift
@@ -7,17 +7,17 @@ var Chessmans: Chessman = ["Пешка":("A",3), "Конь":("G",5), "Слон":
 if let coord = Chessmans["Пешка"]!{
     print("Coordinates of Пешка is \(coord) ")
 }else{
-    print("Error")
+    print("Пешка doesn't exist")
 }
 
 if let coord = Chessmans["Конь"]!{
     print("Coordinates of Конь is \(coord) ")
 }else{
-    print("Error")
+    print("Конь doesn't exist")
 }
 
 if let coord = Chessmans["Слон"]!{
     print("Coordinates of Слон is \(coord) ")
 }else{
-    print("Error")
+    print("Слон doesn't exist")
 }
